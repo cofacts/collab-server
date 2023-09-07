2bd09ef8472e0acb8df7644171fc5bf5f6f2f69f
Nonumpa <acerxp511@msn.com>
2023-09-07 13:08:40 +0800

    Refactor ydoc versions, store it in elasticsearch
    - binary data stores in base64 string
    - rename version.date to version.createdAt

--- src/snapshot.ts
+++ src/snapshot.ts
@@ -1,5 +1,7 @@
-import { Extension, onDisconnectPayload } from '@hocuspocus/server';
+import { Extension, onDisconnectPayload, Document } from '@hocuspocus/server';
 import * as Y from 'yjs';
+import elasticsearch from '@elastic/elasticsearch';
+import 'dotenv/config';
 
 export class Snapshot implements Extension {
   async onDisconnect(data: onDisconnectPayload) {
@@ -7,34 +9,33 @@ export class Snapshot implements Extension {
   }
 }
 
+const elasticsearchOpts: elasticsearch.ClientOptions = {
+  node: process.env.ELASTICSEARCH_URL,
+};
+
+const db = new elasticsearch.Client(elasticsearchOpts);
+
 /**
  * Add a snapshot for current doc
  * ref: https://github.com/yjs/yjs-demos/blob/d8e33e619d6f2da0fae0c6a361286e6901635a9b/prosemirror-versions/prosemirror-versions.js#L26
- * @param {Y.Doc} doc
+ * @param {Document} doc
  */
-export const addVersion = (doc: Y.Doc) => {
-  const versions: Y.Array<{
-    date: number;
-    snapshot: Uint8Array;
-  }> = doc.getArray('versions');
+export const addVersion = async (doc: Document) => {
+  const versions: { createdAt: string; snapshot: string }[] = await getVersion(doc);
+  console.log(versions);
   const prevVersion: {
-    date: number;
-    snapshot: Uint8Array;
-  } = versions.length === 0 ? null : versions.get(versions.length - 1);
+    createdAt: string;
+    snapshot: string;
+  } = versions.length === 0 ? null : versions[versions.length - 1];
+  console.log(prevVersion);
   const prevSnapshot =
     prevVersion === null
       ? Y.emptySnapshot
-      : Y.decodeSnapshot(prevVersion.snapshot);
+      : Y.decodeSnapshot(Buffer.from(prevVersion.snapshot, 'base64'));
   const snapshot = Y.snapshot(doc);
 
   if (!equalSnapshots(doc, prevSnapshot, snapshot)) {
-    // console.log('current clientID: ', doc.clientID);
-    versions.push([
-      {
-        date: new Date().getTime(),
-        snapshot: Y.encodeSnapshot(snapshot),
-      },
-    ]);
+    pushVersion(doc.name, new Date().toISOString(), Y.encodeSnapshot(snapshot));
   }
 };
 
@@ -50,4 +51,70 @@ export const equalSnapshots = (
     doc1.getXmlFragment('prosemirror').toString() ===
     doc2.getXmlFragment('prosemirror').toString()
   );
+};
+
+export const getVersion = async (
+  doc: Document
+): Promise<{ createdAt: string; snapshot: string }[]> => {
+  try {
+    // const db = new elasticsearch.Client(elasticsearchOpts);
+    const result = await db?.getSource({
+      index: 'ydocs',
+      id: doc.name,
+      type: 'doc',
+      _source_includes: ['versions'],
+    });
+
+    return result.body?.versions || [];
+  } catch (e) {
+    if (!e.meta) {
+      console.error(e);
+    } else if (e.meta.statusCode !== 404) {
+      console.error(JSON.stringify(e));
+    }
+    return [];
+  }
+};
+
+export const pushVersion = async (
+  docName: string,
+  createdAt: string,
+  snapshot: Uint8Array
+) => {
+  try {
+    const s = Buffer.from(snapshot).toString('base64');
+
+    const version: { createdAt: string; snapshot: string } = {
+      createdAt,
+      snapshot: s,
+    };
+    await db.update({
+      index: 'ydocs',
+      type: 'doc',
+      id: docName,
+      body: {
+        script: {
+          source: `
+              if(ctx._source.versions == null) {
+                ctx._source.versions = [];
+              }
+              ctx._source.versions.add(params.version);
+            `,
+          params: {
+            // elasticsearch stores binary as a Base64 encoded string
+            // https://www.elastic.co/guide/en/elasticsearch/reference/current/binary.html
+            version,
+          },
+          lang: 'painless',
+        },
+        refresh: true,
+      },
+    });
+  } catch (e) {
+    if (!e.meta) {
+      console.error(e);
+    } else if (e.meta.statusCode !== 404) {
+      console.error(JSON.stringify(e));
+    }
+  }
 };
