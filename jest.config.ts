400ec5960e4214de58458b697c2051a97bef3ed7
Nonumpa <acerxp511@msn.com>
2023-11-29 00:13:21 +0800

    Jest coverage should ignore test folder

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   // modulePaths: [`<rootDir>/test/utils`],
+  setupFilesAfterEnv: ['./test/setup.ts'],
+  coveragePathIgnorePatterns: ['./test/'],
   globals: {
     'ts-jest': {
       tsconfig: './tsconfig.json',
